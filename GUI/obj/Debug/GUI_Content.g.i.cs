425ef2c45c50e9bc2168ebb8b50ced46b45b3baa
Your Name <you@example.com>
2024-05-02 14:15:08 +0700

    Xong giao diện

--- GUI/obj/Debug/GUI_Content.g.i.cs
+++ GUI/obj/Debug/GUI_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon_book.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("view/light%20010.png")]
 
 
