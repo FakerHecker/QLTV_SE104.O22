d84cef6ddc76525f8dfafc4fb3a30905df01e76f
Your Name <you@example.com>
2024-05-13 22:44:35 +0700

    Tạo sơ đồ 3 lớp, code r mà commit bị lỗi nên tự xóa hết :<

---
+++ QuanLyThuVien/QuanLyThuVien/DAO/DataProvider.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QuanLyThuVien.DAO
+{
+    public class DataProvider
+    {
+        private static DataProvider instance;
+
+        public static DataProvider Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new DataProvider();
+                return instance;
+            }
+            private set => instance = value;
+        }
+
+        private DataProvider() { }
+
+        /* public string IncreasePrimaryKey(string primaryKey)
+         {
+             string temp;
+             string number = primaryKey.Substring(2);
+             int num = int.Parse(number);
+             num++;
+             if (num < 10)
+                 temp = "LDG00" + num.ToString();
+             else if (num < 100)
+                 temp = "LDG0" + num.ToString();
+             else
+                 temp = "LDG" + num.ToString();
+             return temp;
+         }*/
+
+        string connectionString = "Data Source=.\\;Initial Catalog=QLTV;Integrated Security=True";
+
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable data = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                adapter.Fill(data);
+            }
+
+            return data;
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            int data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                data = command.ExecuteNonQuery();
+            }
+
+            return data;
+        }
+
+        public object ExecuteScalar(string query)
+        {
+            object data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                data = command.ExecuteScalar();
+            }
+
+            return data;
+        }
+
+    }
+}
